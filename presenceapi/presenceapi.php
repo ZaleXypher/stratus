869ec1bec6fca46639ca0d0d3337c9c0b727fce1
ZaleXypher <89084014+ZaleXypher@users.noreply.github.com>
2024-11-21 07:48:06 +0700

    Update presenceapi.php

--- presenceapi/presenceapi.php
+++ presenceapi/presenceapi.php
@@ -10,7 +10,6 @@
     
     <body>
         <?php
-            //DB INFO
             $config = include('config/config.php');
             $host = $config['host'];
             $port = $config['port'];
@@ -19,12 +18,9 @@
             $password = $config['password'];
             echo $host;
 
-            //CONNECTING TO THE DB
             $conn = pg_connect("host=$host dbname=$dbname user=$user password=xiid");
             $stat = pg_connection_status($conn);
             
-
-            //TABLE CREATION BASED ON DATE
             $today = new DateTime("now", new DateTimeZone('Asia/Jakarta'));
             $month = strtolower($today->format('F_Y'));
             $day = strtolower($today->format('d'));
@@ -35,11 +31,7 @@
                 EXECUTE 'CREATE TABLE IF NOT EXISTS presence.' || quote_ident($1) || ' (kelas TEXT, absen INT, nama TEXT, id varchar);';
             END
             $$ LANGUAGE plpgsql;");
-            
-            pg_query_params($conn,'SELECT create_table($1)', array($month));
 
-            //AUTO-HASH
-            //HACK:AI GENERATED
             pg_query($conn, "CREATE OR REPLACE FUNCTION generate_hash(text) RETURNS VOID AS $$
             DECLARE
                 id TEXT;
@@ -51,12 +43,6 @@
             END
             $$ LANGUAGE plpgsql;");
             
-            pg_query_params($conn, 'SELECT generate_hash($1)', array('studentlist'));
-
-            //STUDENT LIST
-            
-            //GET ROW COUNT
-            //HACK:AI GENERATED, ONLY SLIGHTLY CHANGED
             pg_query($conn, "CREATE OR REPLACE FUNCTION countrow(text) RETURNS VOID AS $$
                 DECLARE
             row_count INT;
@@ -67,55 +53,45 @@
             $$ LANGUAGE plpgsql;");
             $result = pg_query_params($conn, 'SELECT countrow($1)', array($month));
             preg_match('/has (.*) rows./', pg_last_notice($conn), $rowCount);
-
-
-
-            //SYNC FUNCTION
+            
             pg_query($conn, "CREATE OR REPLACE FUNCTION syncnames(text) RETURNS VOID AS $$
             BEGIN
                 EXECUTE 'INSERT INTO presence.' || quote_ident($1) || '(kelas, absen, nama, id) SELECT kelas,absen,nama,id FROM presence.studentlist;';
             END;
             $$ LANGUAGE plpgsql;");
             
-            if ($rowCount[1] == 0) {
-                pg_query_params($conn, 'SELECT syncnames($1)', array($month));
-            }
-           
-
-            
-            //AUTOMATIC COLUMN CREATION BASED ON DATE
             pg_query($conn, "CREATE OR REPLACE FUNCTION add_column(text, text) RETURNS VOID AS $$
             BEGIN   
                 EXECUTE 'ALTER TABLE presence.' || quote_ident($1) ||' ADD COLUMN IF NOT EXISTS ' || quote_ident($2) || 'TEXT';
             END;
             $$ LANGUAGE plpgsql;");
 
-            pg_query_params($conn,'SELECT add_column($1, $2)', array($month, $day));
-            
-
-
-            //CREATE PRESENCE LIST        
             pg_query($conn, "CREATE OR REPLACE FUNCTION checkpresence(text, text) RETURNS VOID AS $$
             BEGIN
                 EXECUTE 'INSERT INTO presence.presencelist (kelas, absen, nama, id, kehadiran) SELECT kelas,absen,nama,id,' || quote_ident($1) || ' FROM presence.' || quote_ident($2) || ';';
             END;
             $$ LANGUAGE plpgsql;");
-                
-                
-                
+            
+            pg_query($conn, "CREATE OR REPLACE FUNCTION checkpresence(text, text) RETURNS VOID AS $$
+            BEGIN
+                EXECUTE 'INSERT INTO presence.presencelist (kelas, absen, nama, id, kehadiran) SELECT kelas,absen,nama,id,' || quote_ident($1) || ' FROM presence.' || quote_ident($2) || ';';
+            END;
+            $$ LANGUAGE plpgsql;");
+
+
+
+
+            pg_query_params($conn,'SELECT create_table($1)', array($month));
+            pg_query_params($conn, 'SELECT generate_hash($1)', array('studentlist'));
+            pg_query_params($conn,'SELECT add_column($1, $2)', array($month, $day));
+            if ($rowCount[1] == 0) {
+                pg_query_params($conn, 'SELECT syncnames($1)', array($month));
+            }
             pg_query($conn, 'DROP TABLE IF EXISTS presence.presencelist CASCADE;');
             pg_query($conn,'CREATE TABLE IF NOT EXISTS presence.presencelist (kelas TEXT, absen INT, nama TEXT, id varchar, kehadiran TEXT);');
             pg_query_params($conn, 'SELECT checkpresence($1, $2)', array($day, $month));
             pg_query($conn, "UPDATE presence.presencelist SET kehadiran = 'Belum Terdata' WHERE kehadiran IS NULL");
 
-
-            //INTERFACE WITH HTTP REQUESTS
-            
-            //ATTENDANCE STATUSES:
-            //1 = PRESENT
-            //2 = LATE
-            //3 = SICK
-            //4 = OTHERS
             if(isset($_GET['presenceid'])) {
                 $presenceid = $_GET['presenceid'];
                 if($_GET['attendancestatus'] == 1){
